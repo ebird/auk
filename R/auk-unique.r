9caa4f3b9939d2dce73ce4810102858ba22f701e
Matt Strimas-Mackey <mstrimas@gmail.com>
2017-05-31 09:44:04 -0400

    fix auk_unique bug

--- R/auk-unique.r
+++ R/auk-unique.r
@@ -8,8 +8,16 @@
 #' @param x data.frame; the EBD data frame, typically as imported by
 #'   [read_ebd()].
 #' @param group_id character; the name of the group ID column.
-#' @param sort_by character; the name of the field to sort by prior to picking
-#'   the first record.
+#' @param checklist_id character; the name of the checklist ID column, each
+#'   checklist within a group will get a unique value for this field. The record
+#'   with the lowest `checklist_id` will be picked as the unique record within
+#'   each group.
+#'
+#' @details This function chooses the checklist within in each that has the
+#'   lowest value for the field specified by `checklist_id`. A new column is
+#'   also created, `checklist_id`, whose value is the taken from the field
+#'   specified in the `checklist_id` parameter for non-group checklists and from
+#'   the field specified by the `group_id` parameter for grouped checklists.
 #'
 #' @return A data.frame with unique observations.
 #' @export
@@ -22,14 +30,14 @@
 #' nrow(ebd)
 #' nrow(ebd_unique)
 auk_unique <- function(x, group_id = "group_identifier",
-                       sort_by = "sampling_event_identifier") {
+                       checklist_id = "sampling_event_identifier") {
   # checks
   assert_that(
     is.data.frame(x),
     assertthat::is.string(group_id),
     group_id %in% names(x),
-    assertthat::is.string(sort_by),
-    sort_by %in% names(x))
+    assertthat::is.string(checklist_id),
+    checklist_id %in% names(x))
 
   # convert group_id to character if not already
   if (!is.character(x[[group_id]])) {
@@ -37,14 +45,18 @@ auk_unique <- function(x, group_id = "group_identifier",
   }
 
   # identify and separate non-group records
-  grouped <- is.na(x[[group_id]])
+  grouped <- !is.na(x[[group_id]])
   x_grouped <- x[grouped, ]
 
   # sort by sampling event id
-  x_grouped <- x_grouped[order(x_grouped[[sort_by]]), ]
+  x_grouped <- x_grouped[order(x_grouped[[checklist_id]]), ]
 
   # remove duplicated records
   x_grouped <- x_grouped[!duplicated(x_grouped[[group_id]]), ]
+
+  # set id field
+  x$checklist_id <- x[[checklist_id]]
+  x_grouped$checklist_id <- x_grouped[[group_id]]
 
   # only keep non-group or non-duplicated records
   rbind(x[!grouped, ], x_grouped)
